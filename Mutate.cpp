f7736d20f5e5422ae8c57ce37f5f3b98e4c46f19
Eric Schulte <schulte.eric@gmail.com>
2013-04-08 20:41:45 -0600

    now counting the number of elements in the program

--- Mutate.cpp
+++ Mutate.cpp
@@ -8,12 +8,39 @@ namespace {
   struct Mutate : public ModulePass {
     static char ID; // pass identification
     Mutate() : ModulePass(ID) {}
-    virtual bool runOnModule(Module &M) {
-      errs() << "ran module pass\n";
-      
-      return false; // true if modified by pass
-    }
+    // consists of functions, global variables, and symbol table entries
+    bool runOnModule(Module &M);
+
+  private:
+    int count;
+    void doFunction(GlobalValue *G);
   };
+}
+
+bool Mutate::runOnModule(Module &M){
+  errs() << "running module pass: ";
+
+  // Module Iterator
+  for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
+    doFunction(I);
+
+  errs() << count << '\n';
+
+  return false; // true if modified by pass
+}
+
+void Mutate::doFunction(GlobalValue *G){
+  if (dyn_cast<GlobalVariable>(G)){
+  } else if (dyn_cast<GlobalAlias>(G)){
+  } else {
+    // this must be a function object.
+    Function *F = cast<Function>(G);
+    
+    for (Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
+      for (BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I)
+        for (User::op_iterator U = I->op_begin(), E = I->op_end(); U != E; ++U)
+          count += 1;
+  }
 }
 
 char Mutate::ID = 0;
