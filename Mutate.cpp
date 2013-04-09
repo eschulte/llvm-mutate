85f4d635bb3b38068dd47de0d97cd5eec49357a9
Eric Schulte <schulte.eric@gmail.com>
2013-04-09 08:15:37 -0600

    re-organize code into separate passes

--- Mutate.cpp
+++ Mutate.cpp
@@ -7,9 +7,6 @@
 
 using namespace llvm;
 
-static cl::opt<std::string>
-Operation("mut-op", cl::desc("Mutation operation to perform"));
-
 static cl::opt<unsigned>
 Stmt1("stmt1", cl::init(0), cl::desc("first statement to mutate"));
 
@@ -17,166 +14,163 @@ static cl::opt<unsigned>
 Stmt2("stmt2", cl::init(0), cl::desc("second statement to mutate"));
 
 namespace {
-  struct Mutate : public ModulePass {
-    static char ID; // pass identification
-    Mutate() : ModulePass(ID) {}
-    // consists of functions, global variables, and symbol table entries
-    bool runOnModule(Module &M);
+  struct Count : public ModulePass {
+    static char ID;
+    Count() : ModulePass(ID) {}
+
+    bool runOnModule(Module &M){
+      count = 0;
+      for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
+        walkFunction(I);
+
+      errs() << count << "\n";
+
+      return false;
+    }
+
+    // We don't modify the program, so we preserve all analyses
+    virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+      AU.setPreservesAll(); }
 
   private:
-    unsigned int count;
-    bool changed_p;
-    void countOp(GlobalValue *G);
-    bool cutOp(GlobalValue *G);
-    bool insertOp(GlobalValue *G);
-    bool swapOp(GlobalValue *G);
+    int unsigned count;
+    void walkFunction(Function *F){
+      for (Function::iterator B = F->begin(), E = F->end(); B != E; ++B) {
+        for (BasicBlock::iterator I = B->begin(), E = B->end(); I != E; ++I) {
+          count += 1; } } }
   };
 }
 
-bool Mutate::runOnModule(Module &M){
-  changed_p = false;
+namespace {
+  struct Cut : public ModulePass {
+    static char ID;
+    Cut() : ModulePass(ID) {}
 
-  if(! strcmp(Operation.c_str(), "count")){
+    bool runOnModule(Module &M){
+      count = 0;
+      changed_p = false;
+      for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
+        if(walkFunction(I)) break;
+
+      if(changed_p) errs() << "cut " << Stmt1 << "\n";
+      else          errs() << "cut failed\n";
+
+      return changed_p; }
+
+  private:
+    int unsigned count;
+    bool changed_p;
+    bool walkFunction(Function *F){
+      for (Function::iterator B = F->begin(), E = F->end(); B != E; ++B) {
+        for (BasicBlock::iterator I = B->begin(), E = B->end(); I != E; ++I) {
+          count += 1;
+          if(count == Stmt1){
+            I->eraseFromParent();
+            changed_p = true;
+            return true; } } }
+      return false; }
+  };
+}
+
+namespace {
+  struct Insert : public ModulePass {
+    static char ID;
+    Insert() : ModulePass(ID) {}
+
+    bool runOnModule(Module &M){
+      count = 0;
+      changed_p = false;
+      for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
+        if(walkCollect(I)) break;
+      count = 0;
+      for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
+        if(walkPlace(I)) break;
+
+      if(changed_p) errs()<<"inserted "<<Stmt2<<" before "<<Stmt1<<"\n";
+      else          errs()<<"insertion failed\n";
+
+      return changed_p; }
+
+  private:
+    int unsigned count;
+    bool changed_p;
+    BasicBlock::iterator temp;
+
+    bool walkCollect(Function *F){
+      for (Function::iterator B = F->begin(), E = F->end(); B != E; ++B) {
+        for (BasicBlock::iterator I = B->begin(), E = B->end(); I != E; ++I) {
+          count += 1;
+          if(count == Stmt2) {
+            temp = I->clone();
+            return true; } } }
+      return false; }
+
+    bool walkPlace(Function *F){
+      for (Function::iterator B = F->begin(), E = F->end(); B != E; ++B) {
+        for (BasicBlock::iterator I = B->begin(), E = B->end(); I != E; ++I) {
+          count += 1;
+          if(count == Stmt1){
+            temp->insertBefore(I);
+            changed_p = true;
+            return true; } } }
+      return false; }
+  };
+}
+
+namespace {
+  struct Swap : public ModulePass {
+    static char ID; // pass identification
+    Swap() : ModulePass(ID) {}
+
     // Count up all operations in the module
-    for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
-      countOp(I);
-    errs() << count << "\n";
-  } else if(! strcmp(Operation.c_str(), "cut")){
-    // Cut Stmt1 from the module
-    for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
-      if(cutOp(I)) break;
-    if(changed_p) errs() << "cut " << Stmt1 << "\n";
-    else          errs() << "cut failed\n";
-  } else if(! strcmp(Operation.c_str(), "insert")){
-    // Insert Stmt1 before Stmt2 in the module
-    for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I) insertOp(I);
-    if(changed_p) errs() << "inserted " << Stmt2 << " before " << Stmt1 << "\n";
-    else          errs() << "insertion failed\n";
-  } else if(! strcmp(Operation.c_str(), "swap")){
-    // Swap Stmt1 with Stmt2 in the module
-    for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
-      if(swapOp(I)) break;
-    if(changed_p) errs() << "swapped " << Stmt1 << " with " << Stmt2 << "\n";
-    else          errs() << "swap failed\n";
-  } else {
-    errs() << "unknown mutation: '" << Operation << "'\n";
-  }
+    bool runOnModule(Module &M){
+      count = 0;
+      changed_p = false;
+      for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
+        walkCollect(I);
+      count = 0;
+      for (Module::iterator I = M.begin(), E = M.end(); I != E; ++I)
+        if(walkPlace(I)) break;
 
-  return changed_p;
-}
+      if(changed_p) errs() << "swapped " << Stmt1 << " with " << Stmt2 << "\n";
+      else          errs() << "swap failed\n";
 
-void Mutate::countOp(GlobalValue *G){
-  if (dyn_cast<GlobalVariable>(G)){
-    // ignore global variables
-  } else if (dyn_cast<GlobalAlias>(G)){
-    // ignore global alias
-  } else {
-    // descend into function objects
-    Function *F = cast<Function>(G);
+      return changed_p; }
 
-    for (Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
-      for (BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I)
-        count += 1;
-  }
-}
-
-bool Mutate::cutOp(GlobalValue *G){
-  if (dyn_cast<GlobalVariable>(G)){
-    // ignore global variables
-  } else if (dyn_cast<GlobalAlias>(G)){
-    // ignore global alias
-  } else {
-    // descend into function objects
-    Function *F = cast<Function>(G);
-
-    for (Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
-      for (BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I) {
-        count += 1;
-        if(count == Stmt1){
-          I->eraseFromParent();
-          changed_p = true;
-          return true;
-        }
-      }
-  }
-  return false;
-}
-
-bool Mutate::insertOp(GlobalValue *G){
-  if (dyn_cast<GlobalVariable>(G)){
-    // ignore global variables
-  } else if (dyn_cast<GlobalAlias>(G)){
-    // ignore global alias
-  } else {
-    // descend into function objects
-    Function *F = cast<Function>(G);
-
-    // collect the instruction
-    BasicBlock::iterator temp1;
-    for (Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
-      for (BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I) {
-        count += 1;
-        if(count == Stmt2){
-          temp1 = I->clone();
-        }
-      }
-
-    // set the instructions
-    count = 0;
-    for (Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
-      for (BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I) {
-        count += 1;
-        if(count == Stmt1){
-          temp1->insertBefore(I);
-          changed_p = true;
-          return true;
-        }
-      }
-  }
-  return false;
-}
-
-bool Mutate::swapOp(GlobalValue *G){
-  if (dyn_cast<GlobalVariable>(G)){
-    // ignore global variables
-  } else if (dyn_cast<GlobalAlias>(G)){
-    // ignore global alias
-  } else {
-    // descend into function objects
-    Function *F = cast<Function>(G);
-
-    // collect the instructions
+  private:
+    int unsigned count;
+    bool changed_p;
     BasicBlock::iterator temp1, temp2;
-    for (Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
-      for (BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I) {
-        count += 1;
-        if(count == Stmt1){
-          temp1 = I->clone();
-        }
-        if (count == Stmt2){
-          temp2 = I->clone();
-        }
-      }
 
-    // set the instructions
-    count = 0;
-    for (Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
-      for (BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; ++I) {
-        count += 1;
-        if(count == Stmt2){
-          ReplaceInstWithInst(I->getParent()->getInstList(), I, temp1);
-          if(changed_p) return true;
-          changed_p = true;
-        }
-        if(count == Stmt1){
-          ReplaceInstWithInst(I->getParent()->getInstList(), I, temp2);
-          if(changed_p) return true;
-          changed_p = true;
-        }
-      }
-  }
-  return false;
+    void walkCollect(Function *F){
+      for (Function::iterator B = F->begin(), E = F->end(); B != E; ++B) {
+        for (BasicBlock::iterator I = B->begin(), E = B->end(); I != E; ++I) {
+          count += 1;
+          if (count == Stmt1) temp1 = I->clone();
+          if (count == Stmt2) temp2 = I->clone(); } } }
+
+    bool walkPlace(Function *F){
+      for (Function::iterator B = F->begin(), E = F->end(); B != E; ++B) {
+        for (BasicBlock::iterator I = B->begin(), E = B->end(); I != E; ++I) {
+          count += 1;
+          if(count == Stmt2){
+            ReplaceInstWithInst(I->getParent()->getInstList(), I, temp1);
+            if(changed_p) return true;
+            changed_p = true;
+          }
+          if(count == Stmt1){
+            ReplaceInstWithInst(I->getParent()->getInstList(), I, temp2);
+            if(changed_p) return true;
+            changed_p = true; } } }
+      return false; }
+  };
 }
 
-char Mutate::ID = 0;
-static RegisterPass<Mutate> X("mutate", "Mutate llvm assembly");
+char Count::ID = 0;
+char Cut::ID = 0;
+char Insert::ID = 0;
+char Swap::ID = 0;
+static RegisterPass<Count>  W("count",  "count the number of instructions");
+static RegisterPass<Cut>    X("cut",    "cut instruction number stmt1");
+static RegisterPass<Insert> Y("insert", "insert stmt2 before stmt1");
+static RegisterPass<Swap>   Z("swap",   "swap stmt1 and stmt2");
