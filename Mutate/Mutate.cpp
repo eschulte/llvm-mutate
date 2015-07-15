8fcc9a516b1aa055a61a89af8de37bd40066d0ef
Eric Schulte <eschulte@grammatech.com>
2015-07-15 18:34:47 +0200

    updated to work with clang/llvm version 3.6.1
      Minor changes to build instructions and included file names.

--- Mutate/Mutate.cpp
+++ Mutate/Mutate.cpp
@@ -3,9 +3,10 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/InstIterator.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
